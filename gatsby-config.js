b24101c655d30fb0c4b5b6f70ec199ef975dfd5d
Rayson Kong <kong.rayson@gmail.com>
2021-01-31 18:08:38 +0800

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `HK Guitar Tutor`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `HKGuitarTutor`,
+    description: `Private Guitar Lesson with Rayson Kong.`,
+    author: `HKGuitarTutor`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
