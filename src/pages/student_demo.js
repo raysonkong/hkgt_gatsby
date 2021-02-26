98009a3f6c99dbbdfd14b1083aacd4facf56c0a3
Rayson Kong <kong.rayson@gmail.com>
2021-02-26 18:11:26 +0800

    add student demos

--- src/pages/student_demo.js
+++ src/pages/student_demo.js
@@ -1,18 +1,154 @@
 import { Link } from "gatsby"
 import React from 'react';
 import Layout from "../components/layout";
+import './student_demo.css'
 
 const studentDemo = () => {
     return (
         <Layout>
             <h1>Grade 8 學生示範</h1>
+
+            <div class="g8">
+                <div>
+                    <h2>Rockschool Grade 8 Meet Darth Ear by Greg</h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/POnAC7H9H00" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div>
+                    <h2>Rockschool Grade 8 Meet Darth Ear by Collins</h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/TvjbXeX4r9U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div>
+                    <h2>Grade 8 Freightshaker By Lung</h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/j6F3Oqp6iDM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div>
+                    <h2>Grade 8 Mind the gaps by Lung</h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/0VJYdUIF2wQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div>
+                    <h2>Grade 8 LeadSheet by Lung</h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/F8kv5uFciNM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div>
+                    <h2>Rockschool Grade 8 Meet Darth Ear by Collins</h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/TvjbXeX4r9U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <h2>其他學生示範</h2>
+
             <div>
-                <h2>Rockschool Grade 8 Meet Darth Ear by Greg</h2>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/POnAC7H9H00" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <h2>Minor Jam by Jason</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/wAHEBxvdmh0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
 
-            
+            <div>
+                <h2>Kiss Etude by Vivian</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/w2Jv9dMrfFk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
 
+            <div>
+                <h2>Larry Carlton Style Blues Solo by Victor</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/KaZY_mT1_a4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Kiss Etude by Ben</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/S1ugKav_ByQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Pentatonic Licks on LA Acoustic Track by Wilson</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/zo7nmGmZJc0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Hair Rock by Charles</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/gRS1ZifQnso" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Minor Pentatonic Licks by Dan</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/j1f8JBk0uCw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Pentatonic Jam By Jason</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/JuqbMlBUtxY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Hair Rock by Frank</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/3FA5qIyvPS8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>A Minor Jam By Hang</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/09u8XrTnsu0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Student Demo: A Minor Jam by Matthew</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/_uyob2GLhJI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Blues Shuffle By Chun</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Jp-EmLJOiAk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>You Give Love a Bad name(Intro & Rhythm) by Jason</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/kFbDVhKWhVo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            
+            <div>
+                <h2>Student Demo: Minor Pentatonic Jam by Alan</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Vi9rqN4ORx8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Student Demo:Brad’s Rag By Hei Hei</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/g13BsW2npfA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2>Student Demo: Pedal To The Metal By Kelvin</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/UPWER2dMjDU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div>
+                <h2></h2>
+
+            </div>
+
+            <div>
+                <h2></h2>
+
+            </div>
+
+            <div>
+                <h2></h2>
+
+            </div>
+
+            <div>
+                <h2></h2>
+
+            </div>
+
+            <div>
+                <h2></h2>
+
+            </div>
+
+            <div>
+                <h2></h2>
+
+            </div>
             <Link to="/">Home</Link> <br />
         </Layout>
     )
