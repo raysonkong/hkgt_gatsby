d6f5cabcae1aad233560f016ea94c517c03014c7
Rayson Kong <kong.rayson@gmail.com>
2021-03-04 17:39:55 +0800

    minor error

--- src/pages/rs_comment.js
+++ src/pages/rs_comment.js
@@ -8,7 +8,6 @@ const RockschoolComment = () => {
             <h2>RockSchool recommended Rayson's Performance  </h2>
             <h1>TWICE!</h1>
 
-            <p>Screen Capture</p>
              <div>
                 <img src="/images/rs_comment_1.jpg" alt="Rockschool Mention" width="1000"/>
             </div>
